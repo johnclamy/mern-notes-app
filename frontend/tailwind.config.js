3e1d4cc532975f2a3b5484b5b3be9fb77d43d3d5
John_Lamy <johnclamy@gmail.com>
2024-08-03 23:57:58 +0100

    set primary app colours

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        app_grey: "#E5E5E5",
+        app_yellow: "#FCA311",
+        app_blue: "#14213D",
+      },
+    },
   },
   plugins: [],
 };
