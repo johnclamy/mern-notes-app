2901b1d50bc6aa8bfcb7cb16111534f22c3d9df2
John_Lamy <johnclamy@gmail.com>
2024-08-04 20:11:27 +0100

    aggregate the top and bottom navbars

---
+++ frontend/src/components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+import NavbarBottom from "./NavbarBottom";
+import NavbarTop from "./NavbarTop";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavbarTop />
+      <NavbarBottom />
+    </nav>
+  );
+};
+
+export default Navbar;
