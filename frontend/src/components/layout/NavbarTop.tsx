edbae9b6d3d1dc423ea276c202b85cde5bc85bb5
John_Lamy <johnclamy@gmail.com>
2024-08-04 20:06:55 +0100

    create the top navigation bar

---
+++ frontend/src/components/layout/NavbarTop.tsx
@@ -0,0 +1,28 @@
+import { FaSearch, FaShoppingBasket } from "react-icons/fa";
+import Logo from "../widgets/Logo";
+
+const NavbarTop = () => (
+  <section className="bg-white border-app_grey dark:bg-gray-900">
+    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+      <a href="/">
+        <Logo />
+      </a>
+      <div className="flex items-center space-x-6 rtl:space-x-reverse">
+        <a href="/">
+          <FaSearch className="text-sm  text-app_blue dark:text-white" />
+        </a>
+        <a href="/">
+          <FaShoppingBasket className="text-sm  text-app_blue dark:text-white" />
+        </a>
+        <a
+          href="/"
+          className="text-sm  text-app_blue dark:text-app_yellow hover:underline"
+        >
+          Login
+        </a>
+      </div>
+    </div>
+  </section>
+);
+
+export default NavbarTop;
