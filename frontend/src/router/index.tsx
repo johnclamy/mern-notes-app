e274bb8baf10e7e596de7b75923ab91662dedcb6
John_Lamy <johnclamy@gmail.com>
2024-08-06 13:26:02 +0100

    add a 404 component to Router in src/router

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -3,6 +3,7 @@ import Home from "../views/Home";
 import Menu from "../views/Menu";
 import MobileApp from "../views/MobileApp";
 import ContactUs from "../views/ContactUs";
+import NoMatch from "../views/NoMatch";
 
 const Router = () => (
   <Routes>
@@ -10,6 +11,7 @@ const Router = () => (
     <Route path="/menu" element={<Menu />} />
     <Route path="/mobile-app" element={<MobileApp />} />
     <Route path="/contact-us" element={<ContactUs />} />
+    <Route path="*" element={<NoMatch />} />
   </Routes>
 );
 
