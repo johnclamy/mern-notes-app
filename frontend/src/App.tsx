72f3261aaf04283cb33a8267e4ad8d5b4358ed30
John_Lamy <johnclamy@gmail.com>
2024-08-06 20:43:17 +0100

    refactor the Routes back to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
-import Router from "./router";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./views/Home";
+import Menu from "./views/Menu";
+import MobileApp from "./views/MobileApp";
+import ContactUs from "./views/ContactUs";
+import NoMatch from "./views/NoMatch";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <Navbar />
-      <Router />
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/mobile-app" element={<MobileApp />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="*" element={<NoMatch />} />
+      </Route>
+    </Routes>
   );
 }
-
-export default App;
