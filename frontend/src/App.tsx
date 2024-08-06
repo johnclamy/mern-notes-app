55168fc6bbccaa0e64b6761b46612a90fb43f370
John_Lamy <johnclamy@gmail.com>
2024-08-06 11:46:29 +0100

    import the router component containing the app routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Router from "./router";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Router />
+      <Footer />
     </>
   );
 }
