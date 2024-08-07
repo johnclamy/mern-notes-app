1210ff634fcd4c4bd58d1c800ded05d95d5722e0
John_Lamy <johnclamy@gmail.com>
2024-08-07 22:55:33 +0100

    add a route for the  Cart and PlaceOrder pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./views/Home";
 import Menu from "./views/Menu";
+import Cart from "./views/Cart";
 import MobileApp from "./views/MobileApp";
 import ContactUs from "./views/ContactUs";
+import PlaceOrder from "./views/PlaceOrder";
 import NoMatch from "./views/NoMatch";
 
 export default function App() {
@@ -14,8 +16,10 @@ export default function App() {
         <Route path="/menu" element={<Menu />} />
         <Route path="/mobile-app" element={<MobileApp />} />
         <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="*" element={<NoMatch />} />
+        <Route path="/cart" element={<Cart />} />
       </Route>
+      <Route path="/place-order" element={<PlaceOrder />} />
+      <Route path="*" element={<NoMatch />} />
     </Routes>
   );
 }
