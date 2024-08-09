5dc95574d137abfafdb340e664535a4675292556
John_Lamy <johnclamy@gmail.com>
2024-08-09 12:06:18 +0100

    prop drill the click handler for HeroButton.tsx

--- frontend/src/components/widgets/HeroButton.tsx
+++ frontend/src/components/widgets/HeroButton.tsx
@@ -1,9 +1,21 @@
-import { ReactNode } from "react";
+import { ReactNode, MouseEventHandler } from "react";
 
-const HeroButton = ({ children }: { children: ReactNode }) => (
-  <button className="mt-8 inline-flex items-center justify-center rounded-xl bg-green-600 py-3 px-6 font-dm text-base font-medium text-white shadow-xl shadow-green-400/75 transition-transform duration-200 ease-in-out hover:scale-[1.02]">
-    {children}
-  </button>
-);
+const HeroButton = ({
+  children,
+  handleClick,
+}: {
+  children: ReactNode;
+  handleClick: MouseEventHandler<HTMLButtonElement>;
+}) => {
+  return (
+    <button
+      type="button"
+      className="mt-8 inline-flex items-center justify-center rounded-xl bg-green-600 py-3 px-6 font-dm text-base font-medium text-white shadow-xl shadow-green-400/75 transition-transform duration-200 ease-in-out hover:scale-[1.02]"
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default HeroButton;
