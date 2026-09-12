f9289d8873e7efcff5c59b62ac73cb00fc2e0f27
johnclamy <johnclamy@gmail.com>
2026-09-12 16:01:31 +0100

    create a logout btn

---
+++ src/components/app/user/Logout.tsx
@@ -0,0 +1,16 @@
+const Logout: React.FC = () => {
+    return (
+        <form onSubmit={(e) => {
+            e.preventDefault()
+        }}>
+            <input
+                type="submit"
+                value="Logout"
+                className="rounded-xl bg-red-500 px-3 py-1.5 text-xs font-medium text-white transition-all hover:bg-red-600 sm:px-4 sm:py-2 sm:text-sm cursor-pointer whitespace-nowrap"
+            />
+        </form>
+    )
+}
+
+
+export default Logout
