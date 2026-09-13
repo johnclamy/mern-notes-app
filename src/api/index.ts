6febbd67c352e4f4c029f9a980ef906c40c9d1f0
johnclamy <johnclamy@gmail.com>
2026-09-13 08:03:28 +0100

    create a mock data list prior to implementing an api

---
+++ src/api/index.ts
@@ -0,0 +1,33 @@
+// Dummies data prior to implementing the API
+import type IPost from "../components/app/post/IPost";
+
+
+const posts: IPost[] = [
+    {
+        title: "My First Post",
+        content: "This is the content of my first post.",
+        author: "John Doe",
+        dateCreated: "2024-06-01",
+    },
+    {
+        title: "My Second Post",
+        content: "This is the content of my second post.",
+        author: "Jane Smith",
+        dateCreated: "2024-06-02",
+    },
+    {
+        title: "My Third Post",
+        content: "This is the content of my third post.",
+        author: "Bob Johnson",
+        dateCreated: "2024-06-03",
+    },
+    {
+        title: "My forth Post",
+        content: "This is the content of my fourth post.",
+        author: "Alice Brown",
+        dateCreated: "2024-06-04",
+    }
+]
+
+
+export default posts
