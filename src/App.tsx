12bf65a57fc5c56f5f696fc7f1ad30e8da2b8a1a
johnclamy <johnclamy@gmail.com>
2026-09-13 08:04:33 +0100

    place the PostList component within the App container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 {/* micro-blog-project/src/App.tsx */}
 import Wrapper from './components/layout/Wrapper'
 import Header from './components/layout/Header'
+import PostList from './components/app/post/PostList'
 
 
 const App: React.FC = () => {
     return (        
         <Wrapper>
             <Header />
-            <p className="col-span-full">content goes here...</p>
+            <PostList />
         </Wrapper>
     )
 }
