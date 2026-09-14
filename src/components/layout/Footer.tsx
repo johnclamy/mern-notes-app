1adedbc1fa74d4d93d095853001751b19c7bc89f
johnclamy <johnclamy@gmail.com>
2026-09-14 22:55:25 +0100

    setup a footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => (
+    <footer className="bg-gray-50 border-t border-gray-200 py-6">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-sm text-gray-500">
+            &copy; {new Date().getFullYear()} micro-post. All rights reserved.
+        </div>
+    </footer>
+)
+
+
+export default Footer
