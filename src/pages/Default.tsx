56359951818135bb232f2bcf363abb4e70ce48cf
johnclamy <johnclamy@gmail.com>
2026-09-15 11:25:05 +0100

    add the list of posts into the default (index.html) page

--- src/pages/Default.tsx
+++ src/pages/Default.tsx
@@ -1,11 +1,9 @@
-const DefaultPage: React.FC = () => {
-    return (
-        <div>
-            <h1>Welcome to the Default Page</h1>
-            <p>This is the default page of the application.</p>
-        </div>
-    )
-}
+{/* micro-post-project/src/pages/Default.tsx */}
+import PostList from '../components/app/post/PostList'
+
+
+const DefaultPage: React.FC = () => <PostList />
+
 
 
 export default DefaultPage
